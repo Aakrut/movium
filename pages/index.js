41653f404cd9a64953b617b8cf5a5d39a7ba9503
aakrut <aakrutdabhi@gmail.com>
2022-07-11 22:45:39 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import HomeHero from './components/sections/homeHero'
-
+import Head from "next/head";
+import Image from "next/image";
+import HomeHero from "./components/sections/homeHero";
 
 export default function Home({ nowPlaying }) {
-
-  return (
-    <>
-      <HomeHero results={ nowPlaying }/>
-    </>
-  );
-}
-
-
-export async function getServerSideProps() {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
-  );
-
-  const data = await response.json();
-
-  return {
-    props: {
-      nowPlaying: data,
-    },
-  };
+  return <></>;
 }
